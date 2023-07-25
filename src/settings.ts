b0b8cd4847f806733d3186256040cef4ae7191b3
postfm <postfm@yandex.ru>
2023-07-25 19:54:12 +0300

    Добавил на карту маркеры по ТЗ

--- src/settings.ts
+++ src/settings.ts
@@ -39,11 +39,9 @@ export const CITY_DEFAULT = 0;
 
 export const EMPTY_RATING = '0';
 
-export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+export const URL_MARKER_DEFAULT = '../public/img/pin.svg';
 
-export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+export const URL_MARKER_CURRENT = '../public/img/pin-active.svg';
 
 export const StylesForMapMainPage = {
   width: '100%',
