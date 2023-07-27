c66bf2af281c4e8ba2e66cce67eaab3852c97dc5
postfm <postfm@yandex.ru>
2023-07-28 00:31:26 +0300

    Revert "Выполнил домашнее задание"
    This reverts commit 9b4f54112dd23247b706cec4be13b43f4b993b36.

--- src/utils/offers.ts
+++ src/utils/offers.ts
@@ -1,9 +1,3 @@
-import { Offers } from '../types/data-types';
-
 export function capitalizeFirstLetter(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-}
-
-export function getCityOffers(offers: Offers, cityName: string) {
-  return offers.filter((offer) => offer.city.name === cityName);
 }
