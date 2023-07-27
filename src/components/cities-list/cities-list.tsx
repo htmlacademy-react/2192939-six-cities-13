9b4f54112dd23247b706cec4be13b43f4b993b36
postfm <postfm@yandex.ru>
2023-07-28 00:30:54 +0300

    Выполнил домашнее задание

--- src/components/cities-list/cities-list.tsx
+++ src/components/cities-list/cities-list.tsx
@@ -1,19 +1,33 @@
-import { CITY_DEFAULT } from '../../settings';
 import { Cities } from '../../types/data-types';
 import classNames from 'classnames';
+import { MouseEvent } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { selectCityAction } from '../../store/action';
+
 
 type CitiesListProps = {
   cities: Cities;
 };
 export default function CitiesList({ cities }: CitiesListProps): JSX.Element {
+  const selectedCity = useAppSelector((state) => state.cityName);
+  const dispatch = useAppDispatch();
+
+  const handleElementClick = (evt: MouseEvent<HTMLElement>) => {
+    evt.preventDefault();
+    if (!evt.currentTarget.textContent) {
+      return;
+    }
+    dispatch(selectCityAction(evt.currentTarget.textContent));
+  };
+
   return (
     <section className="locations container">
       <ul className="locations__list tabs__list">
-        {cities.map((city, index) => (
-          <li className="locations__item" key={crypto.randomUUID()}>
+        {cities.map((city) => (
+          <li className="locations__item" key={city} onClick={handleElementClick}>
             <a
               className={classNames('locations__item-link', 'tabs__item', {
-                'tabs__item--active': index === CITY_DEFAULT,
+                'tabs__item--active': city === selectedCity,
               })}
             >
               <span>{city}</span>
