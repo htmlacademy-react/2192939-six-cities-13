3498f6f983afb6c0f6121da337f05e8be1acdba6
postfm <postfm@yandex.ru>
2023-07-28 11:45:13 +0300

    Разделил на компоненты

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -4,11 +4,13 @@ import Header from '../../components/header';
 import { AuthStatus, CITIES, StylesForMapMainPage } from '../../settings';
 import CitiesList from '../../components/cities-list';
 import Map from '../../components/map';
-import PlaceCard from '../../components/place-card';
 import { useState } from 'react';
 import { useAppSelector } from '../../hooks';
 import { getCityOffers } from '../../utils/offers';
 import classNames from 'classnames';
+import PlaceListEmpty from '../../components/place-list-empty';
+import Sorting from '../../components/sorting';
+import PlaceList from '../../components/place-list';
 
 type MainPageProps = {
   offers: Offers;
@@ -49,15 +51,7 @@ export default function MainPage({
         </div>
         <div className="cities">
           {!cityOffers.length &&
-            <div className="cities__places-container cities__places-container--empty container">
-              <section className="cities__no-places">
-                <div className="cities__status-wrapper tabs__content">
-                  <b className="cities__status">No places to stay available</b>
-                  <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
-                </div>
-              </section>
-              <div className="cities__right-section"></div>
-            </div>}
+            <PlaceListEmpty />}
           {cityOffers.length &&
             <div className="cities__places-container container">
               <section className="cities__places places">
@@ -65,47 +59,17 @@ export default function MainPage({
                 <b className="places__found">
                   {cityOffers.length} places to stay in {cityName}
                 </b>
-                <form className="places__sorting" action="#" method="get">
-                  <span className="places__sorting-caption">Sort by</span>
-                  <span> </span>
-                  <span className="places__sorting-type" tabIndex={0}>
-                    Popular
-                    <svg className="places__sorting-arrow" width={7} height={4}>
-                      <use xlinkHref="#icon-arrow-select" />
-                    </svg>
-                  </span>
-                  <ul className="places__options places__options--custom places__options--opened">
-                    <li
-                      className="places__option places__option--active"
-                      tabIndex={0}
-                    >
-                      Popular
-                    </li>
-                    <li className="places__option" tabIndex={0}>
-                      Price: low to high
-                    </li>
-                    <li className="places__option" tabIndex={0}>
-                      Price: high to low
-                    </li>
-                    <li className="places__option" tabIndex={0}>
-                      Top rated first
-                    </li>
-                  </ul>
-                </form>
-                <div className="cities__places-list places__list tabs__content" >
-                  {
-                    cityOffers.map((offer) => (
-                      <PlaceCard key={offer.id} offer={offer} type={'cities'}
-                        onCardEnter={handleMouseEnter}
-                        onCardLeave={handleMouseLeave}
-                      />
-                    ))
-                  }
-                </div >
+                <Sorting />
+                <PlaceList offers={cityOffers} type={'cities'}
+                  onCardEnter={handleMouseEnter}
+                  onCardLeave={handleMouseLeave}
+                />
               </section>
               <div className="cities__right-section">
                 <section className="cities__map map">
-                  <Map city={cityOffers[0].city} offers={cityOffers} styles={StylesForMapMainPage} selectedPlace={activeCard} />
+                  <Map city={cityOffers[0].city} offers={cityOffers}
+                    styles={StylesForMapMainPage} selectedPlace={activeCard}
+                  />
                 </section>
               </div>
             </div>}
