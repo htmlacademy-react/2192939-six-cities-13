d256cc1016e0ab2b7417bdd301f07858b859d320
postfm <postfm@yandex.ru>
2023-08-05 15:42:18 +0300

    Восстановил очистку формы отправки отзыва

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -41,8 +41,8 @@ export default function ReviewForm({ offerId }: ReviewFormProps): JSX.Element {
       rating: rating,
     }));
 
-    // setRating(0);
-    // setReview('');
+    setRating(0);
+    setReview('');
   };
 
   return (
