02fb62e6088bce08d42060ca41f0bac813eeb94d
postfm <postfm@yandex.ru>
2023-08-06 19:10:13 +0300

    Удалил управление скроллом

--- src/components/place-list/place-list.tsx
+++ src/components/place-list/place-list.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames';
 import { Offers } from '../../types/data-types';
 import PlaceCard from '../place-card';
-import { useRef, useEffect } from 'react';
 
 type PlaceListProps = {
   offers: Offers;
@@ -12,19 +11,11 @@ type PlaceListProps = {
 }
 
 export default function PlaceList({ offers, type, onCardEnter, onCardLeave }: PlaceListProps): JSX.Element {
-  const elementRef = useRef<HTMLDivElement>(null);
-
-  useEffect(
-    () => {
-      elementRef.current?.scrollTo(0, 0);
-    }
-  );
-
   return (
     <div className={classNames(
       { 'cities__places-list places__list tabs__content': type === 'cities' },
       { 'near-places__list places__list': type === 'near-places' },
-      { 'favorites__places': type === 'favorites' })} ref={elementRef}
+      { 'favorites__places': type === 'favorites' })}
     >
       {
         offers.map((offer) => (
