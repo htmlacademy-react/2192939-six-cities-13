a97df10f4d81c1646114dfcbfaead1edd6d54a08
postfm <postfm@yandex.ru>
2023-08-07 19:08:00 +0300

    Добавил useCallback

--- src/components/place-with-sorting/place-with-sorting.tsx
+++ src/components/place-with-sorting/place-with-sorting.tsx
@@ -5,6 +5,7 @@ import { PlacesCard } from '../../settings';
 import { getSortedOffersBy } from '../../utils/offers';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { setSortingType } from '../../store/action';
+import { useCallback } from 'react';
 
 
 type PlaceWithSortingProps = {
@@ -15,9 +16,9 @@ export default function PlaceWithSorting({ cityOffers }: PlaceWithSortingProps) 
   const dispatch = useAppDispatch();
   const typeSorting = useAppSelector((store) => store.sortingType);
 
-  function handleChangeSorting(sortType: SortingType) {
+  const handleChangeSorting = useCallback((sortType: SortingType) => {
     dispatch(setSortingType(sortType));
-  }
+  }, [dispatch]);
 
   return (
     <>
