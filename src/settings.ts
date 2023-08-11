8a72f8b84a21f0582c112969900cca512c9a903d
postfm <postfm@yandex.ru>
2023-08-11 16:49:57 +0300

    Длинные строки заменил константами

--- src/settings.ts
+++ src/settings.ts
@@ -103,3 +103,7 @@ export enum NameSpace {
   Data = 'DATA',
   User = 'USER',
 }
+
+export const MAP_URL_TEMPLATE = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
+
+export const MAP_OPTIONS_ATTRIBUTION = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
