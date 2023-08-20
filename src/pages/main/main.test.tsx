1b029add04b94487e4d0d7d1e11632a7155e10d1
postfm <postfm@yandex.ru>
2023-08-20 15:54:11 +0300

    Добавит тест для главной страницы

---
+++ src/pages/main/main.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+import { withHistory, withStore } from '../../test-mocks/test-component';
+import MainPage from '.';
+import { AuthStatus, DEFAULT_CITY, DEFAULT_SORTING, Status } from '../../settings';
+import { FullOffer } from '../../types/data-types';
+
+describe('Component: MainPage', () => {
+  it('Проверяем правильность отрисовки', () => {
+    const expectedText = 'Cities';
+    const { withStoreComponent } = withStore(<MainPage />, {
+      DATA: {
+        offers: [],
+        fullOffer: {} as FullOffer,
+        reviews: [],
+        neighborPlaces: [],
+        favorites: [],
+        isOffersDataLoading: false,
+        isFullOfferDataLoading: true,
+        isReviewsDataLoading: true,
+        isNeighborPlacesDataLoading: true,
+        isFavoritesLoading: false,
+        isFavoriteAdding: false,
+        hasError: false,
+        currentCityName: DEFAULT_CITY,
+        activeCard: null,
+        sortingType: DEFAULT_SORTING,
+        statusReview: Status.Idle,
+      },
+      USER: {
+        authStatus: AuthStatus.Unknown,
+        loginStatus: Status.Idle,
+        userName: '',
+      }
+    });
+    const preparedComponent = withHistory(withStoreComponent);
+
+    render(preparedComponent);
+
+    expect(screen.getByText(expectedText)).toBeInTheDocument();
+  });
+});
