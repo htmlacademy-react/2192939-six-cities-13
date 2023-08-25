33ef9d0168213e4f0e2d546b72c86937a6be9d10
postfm <postfm@yandex.ru>
2023-08-25 03:22:09 +0300

    Исправит тест

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -2,9 +2,8 @@ import { render, screen } from '@testing-library/react';
 import { MemoryHistory, createMemoryHistory } from 'history';
 import { AppRoute, DEFAULT_CITY, DEFAULT_SORTING, Status, TIME_TO_RENDER_PAGE } from '../../settings';
 import { withHistory, withStore } from '../../test-mocks/test-component';
-import { makeFakeFullOffer, makeFakeOffer, makeFakeStore } from '../../test-mocks/test-mocks';
+import { makeFakeFullOffer, makeFakeStore } from '../../test-mocks/test-mocks';
 import App from '.';
-import { FullOffer } from '../../types/data-types';
 
 describe('Маршрутизация приложения', () => {
   let mockHistory: MemoryHistory;
